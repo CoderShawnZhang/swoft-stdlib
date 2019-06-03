60d8a251afacc83249be27856238572b95c14f7e
zhanghongbo <taobao1875@163.com>
2019-06-03 09:08:05 +0800

    init

---
+++ src/Stdlib.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghongbo
+ * Date: 2019/6/3
+ * Time: 上午9:06
+ */
+namespace Swoft\Stdlib;
+
+class Stdlib
+{
+    public function test()
+    {
+        echo "run Stdlib";
+    }
+}
